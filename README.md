871fb16142fa3bdb555903190c94b9de7205daec
Luis <luis.almajano@gmail.com>
2025-02-28 17:48:22 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## How to Use Queues in Web Applications
+
+### Node.js and Redis Tutorial (Bull Library)
+
+### Scope
+
+ We'll build a simple project using Node.js and Redis. We'll use the Bull library as it simplifies a lot of the complexities involved in building a queue system. The project will have a single endpoint to send emails.
+
+- Usage of Bull library: https://www.npmjs.com/package/bull
+- Both Javascript and Typescript variants of the code (see also package.json)
+- Typescript Execute (txs): https://www.npmjs.com/package/tsx
+
+
+### Reference:
+
+https://www.freecodecamp.org/news/how-to-use-queues-in-web-applications/
---
+++ src/app.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+import Bull, { Job } from "bull";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Bull Queue in Redis
+const emailQueue = new Bull("email");
+
+
+// Producer
+const sendNewEmail = async (email) => {
+  emailQueue.add({ ...email });
+};
+
+// Worker
+const processEmailQueue = async (job) => {
+  //Use a test account as this is a tutorial
+  const testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  const { from, to, subject, text } = job.data;
+
+  console.log("Sending mail to %s...", to);
+
+  let info = await transporter.sendMail({
+    from,
+    to,
+    subject,
+    text,
+    html: `<strong>${text}</strong>`,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  return nodemailer.getTestMessageUrl(info);
+};
+
+// Connect the Worker to the queue
+emailQueue.process(processEmailQueue);
+
+app.post("/send-email", async (req, res) => {
+  const { from, to, subject, text } = req.body;
+
+  await sendNewEmail({ from, to, subject, text });
+  console.log("Email added to queue");
+
+  res.json({
+    message: "Email sent",
+  });
+});
+
+app.listen(4300, () => {
+  console.log("Server started at http://localhost:4300");
+});
